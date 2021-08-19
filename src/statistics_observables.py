5051531053723e3ecb3ca5f8c42015011bf207c1
Ilya <ilyakudrov@yandex.ru>
2021-08-19 18:55:32 +0300

    jackknife and bootstrap with numbda added

---
+++ src/statistics_observables.py
@@ -0,0 +1,84 @@
+import math
+import numpy as np
+from numba import njit, float64
+
+# @njit
+def mysum(xs):
+    tmp = 0
+    for x in xs:
+        tmp += x
+
+    return tmp
+
+# @njit
+def jackknife(x, func):
+    n = x.shape[1]
+    idx = np.arange(n)
+    return mysum([func(x[:, idx != i]) for i in range(n)])/float(n)
+
+# @njit
+def jackknife_var(x, func):
+    n = x.shape[1]
+    idx = np.arange(n)
+    j_est = jackknife(x, func)
+    return j_est, (n-1)/(n + 0.0) * mysum([(func(x[:, idx != i]) - j_est)**2.0
+                                        for i in range(n)])
+
+@njit
+def bootstrap_smaple_generate(x, func, K):
+    n = x.shape[1]
+    m = x.shape[0]
+    y = np.zeros((m, K))
+
+    for k in range(m):
+        for i in range(K):
+            for j in range(n):
+                y[k][i] += x[k][np.random.randint(0, n)]
+
+    for k in range(m):
+        for i in range(K):
+            y[k][i] = y[k][i] / n
+
+    return func(y)
+
+
+@njit
+def bootstrap_numba(x, func, K):
+    sample = bootstrap_smaple_generate(x, func, K)
+    estimate = sample.mean()
+    sigma = 0
+    n = sample.shape[0]
+    for i in range(n):
+        sigma += (sample[i] - estimate)**2
+    return estimate, math.sqrt((n-1)/(n + .0) * sigma)
+
+
+@njit
+def jackknife_sample_generate(x, func):
+    m = x.shape[0]
+    n = x.shape[1]
+    sum_x = np.zeros(m)
+    y = np.zeros((m, n))
+    for i in range(n):
+        for j in range(m):
+            sum_x[j] += x[j][i]
+
+    for i in range(n):
+        for j in range(m):
+            y[j][i] = (sum_x[j] - x[j][i])/(n-1)
+
+    return func(y)
+
+@njit
+def jackknife_var_numba(x, func):
+    n = x.shape[1]
+    x1 = jackknife_sample_generate(x, func)
+    j_est = x1.mean()
+    sigma = 0
+    for i in range(n):
+        sigma += (x1[i] - j_est)**2
+    return j_est, math.sqrt((n-1)/(n + .0) * sigma)
+
+@njit
+def average(x):
+    return x[0]
---
+++ test/code/get_result_potential.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+import time
+import math
+import sys
+import os.path
+from numba import njit, float64
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".."))
+import src.statistics_observables as stat
+from astropy.stats import jackknife_resampling, jackknife_stats, bootstrap
+
+def get_field(data, df1, df, time_size_max):
+
+    time_size = data["T"].iloc[0]
+    space_size = data["r/a"].iloc[0]
+
+    # print("time_size", time_size)
+    # print("space_size", space_size)
+
+    if time_size < time_size_max:
+
+        x1 = data['wilson_loop'].to_numpy()
+
+        x2 = df[(df["T"] == time_size + 1) & (df["r/a"] == space_size)]['wilson_loop'].to_numpy()
+
+        x3 = np.vstack((x1, x2))
+
+        # field, err = stat.jackknife_var(x3, potential)
+        field, err = stat.jackknife_var_numba(x3, potential_numba)
+
+        # print("my jackknife", stat.jackknife_var_numba(x1.reshape((1, x1.shape[0])), stat.average))
+
+        # print("astropy jackknife", jackknife_stats(x1, np.mean, 0.95))
+
+        # print("my bootstrap", stat.bootstrap_numba(x1.reshape((1, x1.shape[0])), stat.average, 100000))
+
+        # print("astropy bootstrap", bootstrap(x1, 1000, bootfunc=np.mean))
+
+        new_row = {'aV(r)': field, 'err': err}
+
+        df1 = df1.append(new_row, ignore_index=True)
+
+        return df1
+
+@njit
+def potential_numba(x):
+    n = x.shape[1]
+    y = np.zeros(n)
+    for i in range(n):
+        fraction = x[0][i]/x[1][i]
+        if(fraction >= 0):
+            y[i] = math.log(fraction)
+        else:
+            y[i] = 0
+    return y
+
+def potential(x):
+    a = np.mean(x, axis = 1)
+    fraction = a[0] / a[1]
+    if(fraction >= 0):
+        return math.log(fraction)
+    else:
+        return 0
+
+mu1 = ['0.05']
+chains = {"s0", "s1", "s2", "s3", "s4"}
+
+for mu in mu1:
+    data = []
+    for chain in chains:
+        for i in range(0, 700):
+            file_path = f"../data/wilson_loop/qc2dstag/40^4/mu{mu}/{chain}/wilson_loop_{i:04}"
+
+            # print(file_path)
+            # print(chain)
+            if(os.path.isfile(file_path)):
+                data.append(pd.read_csv(file_path, header = 0, names=["T", "r/a", "wilson_loop"]))
+                data[-1]["conf_num"] = i
+
+    df = pd.concat(data)
+
+    # df = df[(df['T'] >= 10) & (df['T'] <= 11)]
+    # df = df[df['r/a'] == 10]
+
+    df1 = pd.DataFrame(columns=["aV(r)", "err"])
+
+    time_size_max = df["T"].max()
+
+    start = time.time()
+
+    df1 = df.groupby(['T', 'r/a']).apply(get_field, df1, df, time_size_max).reset_index()
+
+    end = time.time()
+    print("execution time = %s" % (end - start))
+
+    df1 = df1[['T', 'r/a', 'aV(r)', 'err']]
+
+    path_output = f"../result/potential/qc2dstag/40^4"
+
+    try:
+        os.makedirs(path_output)
+    except:
+        pass
+
+    df1.to_csv(f"{path_output}/potential1_mu={mu}.csv", index=False)
